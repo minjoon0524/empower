b50216158690f1c119e010994f28a8e9b31ca481
MINJUN YU <minjoon0524@gmail.com>
2024-09-19 20:24:14 +0900

    feat:CategoryMenu 초기설정

--- src/menus/CategoryMenu.jsx
+++ src/menus/CategoryMenu.jsx
@@ -1,12 +1,51 @@
-import React from 'react'
-import style from './Menu.module.css'
+import React from "react";
+import style from "./Menu.module.css";
 const CategoryMenu = () => {
   return (
     <div className={style.category_container}>
- <i className="blind">좌측메뉴 토글</i>
-      
+      <div className={style.category_item}>
+        <div className={style.menu_box}>
+          <strong className={style.menu_section_title}>공통 관리</strong>
+          <ul class="menu_list">
+            <li class="general">
+              <a data-lwds-atom="true" className={style.item_txt} href="#">
+                인사관리
+              </a>
+              <ul className={style.sub_menu}>
+                <li class="">
+                  <a data-lwds-atom="true" className={style.sub_item} href="#">
+                    직원조회
+                  </a>
+                </li>
+                <li class="">
+                  <a data-lwds-atom="true" className={style.sub_item} href="#">
+                    맞춤 설정
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li class="member">
+              <a data-lwds-atom="true" className={style.item_txt} href="#">
+                근태관리
+              </a>
+              <ul className={style.sub_menu}>
+                <li class="">
+                  <a data-lwds-atom="true" className={style.sub_item} href="#">
+                    구성원
+                  </a>
+                </li>
+                <li class="">
+                  <a data-lwds-atom="true" className={style.sub_item} href="#">
+                    구성원 초대
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CategoryMenu
+export default CategoryMenu;
