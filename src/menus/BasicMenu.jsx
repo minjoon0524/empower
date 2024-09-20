bca0969df8660cade4c201f30ca872a36181ddc1
MINJUN YU <minjoon0524@gmail.com>
2024-09-20 19:31:11 +0900

    design: 사용자 이름과 로그아웃 버튼이 나란하게 수정

--- src/menus/BasicMenu.jsx
+++ src/menus/BasicMenu.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import style from "./Menu.module.css";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../slices/loginSlice";
+import { useNavigate } from "react-router-dom";
 const BasicMenu = () => {
-// BasicMenu가 관심있는 것은 로그인 됐는지 안 됐는지에 대한 상태
-//=>로그인 Slices의 상태
-
-const loginState = useSelector((state) => state.loginSlice);
-console.log("loginState Test---------",loginState)  
-return (
+  // BasicMenu가 관심있는 것은 로그인 됐는지 안 됐는지에 대한 상태
+  //=>로그인 Slices의 상태
+  const disPatch = useDispatch();
+  const navigate=useNavigate();
+  const loginState = useSelector((state) => state.loginSlice);
+  const handleClickLogout = (event) => {
+    event.preventDefault(); // 기본 동작 방지
+    disPatch(logout());
+    navigate('/login')
+  };
+  console.log("loginState Test---------", loginState);
+  return (
     <nav className={style.container}>
       <div className={style.nav_item}>
         <a className={style.title}>Empower</a>
@@ -22,7 +30,11 @@ return (
               />
             </button>
           </div>
-          <div>개발1팀 홍길동</div>
+          <div className={style.user_area}>
+              <span className={style.user_name}>개발1팀 홍길동</span>
+              <a className={style.logout_button} onClick={handleClickLogout}>로그아웃</a>
+            </div>
+
         </div>
       </div>
     </nav>
