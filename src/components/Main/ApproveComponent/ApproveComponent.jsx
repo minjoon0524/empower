9bc094ac33be8f9c9ccf502d5e27d3996675e2b1
MINJUN YU <minjoon0524@gmail.com>
2024-09-30 17:57:27 +0900

    feat:가입승인을 위한 컴포넌트(axios 호출 완료)

--- src/components/Main/ApproveComponent/ApproveComponent.jsx
+++ src/components/Main/ApproveComponent/ApproveComponent.jsx
@@ -1,37 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import style from "./Approve.style.css";
 import useCustomLogin from './../../../hooks/useCustomLogin';
+import { getMemberList } from "../../../api/memberApi";
+import useCustomMove from "../../../hooks/useCustomMove";
+
+const initState = {
+  dtoList: [],
+  pageNumList: [],
+  pageRequestDTO: null,
+  prev: false,
+  next: false,
+  totalCount: 0,
+  prevPage: 0,
+  nextPage: 0,
+  totalPage: 0,
+  current: 0,
+};
 
 const ApproveComponent = () => {
-  // 로그인한 User확인을 위한 State
-  const {loginState}=useCustomLogin();
-  console.log(loginState)
-  return (
-    
-    <table>
-    <thead>
-      <tr>
-        <th>이름</th>
-        <th>부서</th>
-        <th>직급</th>
-        <th>이메일</th>
-        <th>전화번호</th>
-        <th>가입승인</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>{loginState.name}</td>
-        <td>{loginState.department}</td>
-        <td>{loginState.position}</td>
-        <td>{loginState.eid}</td>
-        <td>{loginState.phone}</td>
-        <td>{loginState.memberCheck}</td>
-      </tr>
+  const { page, size,refresh,moveToList,moveToRead } = useCustomMove();
 
-    </tbody>
-  </table>
-);
+  const [serverData, setServerData] = useState(initState);
+
+
+  useEffect(() => {
+    getMemberList({ page, size }).then((data) => {
+      console.log(data);
+      setServerData(data);
+    });
+  }, [page, size,refresh]);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>이름</th>
+          <th>부서</th>
+          <th>직급</th>
+          <th>이메일</th>
+          <th>전화번호</th>
+          <th>가입승인</th>
+        </tr>
+      </thead>
+      <tbody>
+      {serverData.dtoList.map((member) => (
+          <tr key={member.eid}>
+            <td>{member.name}</td>
+            <td>{member.department}</td>
+            <td>{member.position}</td>
+            <td>{member.email}</td>
+            <td>{member.phone}</td>
+            <td>{member.memberCheck ? "승인" : "미승인"}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 };
 
 export default ApproveComponent;
