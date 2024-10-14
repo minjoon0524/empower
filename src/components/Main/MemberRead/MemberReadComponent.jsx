76464e04c3749179f46402acb94b37c64209c361
MINJUN YU <minjoon0524@gmail.com>
2024-10-14 21:49:10 +0900

    design: 회원 조회 컴포넌트 디자인 수정 및 회원 수정,삭제 버튼 추가

--- src/components/Main/MemberRead/MemberReadComponent.jsx
+++ src/components/Main/MemberRead/MemberReadComponent.jsx
@@ -1,49 +1,122 @@
-import React, { useEffect,useState } from 'react'
-import { getMember } from '../../../api/memberApi';
-import styles from './MemberReadComponent.module.css';
+import React, { useEffect, useState } from "react";
+import { getMember } from "../../../api/memberApi";
+import styles from "./MemberReadComponent.module.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBuilding, faCalendarDays, faEnvelope, faLocationDot, faPhone, faRankingStar } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 
+const initState = {
+  eid: "",
+  name: "",
+  department: "",
+  email: "",
+  phone: "",
+  address: "",
+  position: "",
+  hireDate: "",
+};
 
-const initState={
-    eid:"",
-    name:"",
-    department:"",
-    email:"",
-    phone:"",
-    address:"",
-    position:"",
-    hireDate:""
-}
-const MemberReadComponent = ({eid}) => {
-    const [member, setMember] = useState(initState);
-    
-    useEffect(()=>{
-        getMember(eid).then((data)=>{
-            console.log(data);
-            setMember(data)
-        })
-    },[eid])
-    return (
+const MemberReadComponent = ({ eid }) => {
+  const [member, setMember] = useState(initState);
+
+  useEffect(() => {
+    getMember(eid).then((data) => {
+      console.log(data);
+      setMember(data);
+    });
+  }, [eid]);
+
+  return (
+    <div>
       <div className={styles.memberCard}>
-        <h2>회원 정보</h2>
-        <p className={styles.memberInfo}>
-          <span className={styles.label}>사원번호:</span> {member.eid}
-        </p>
-        <p className={styles.memberInfo}>
-          <span className={styles.label}>주소:</span> {member.address}
-        </p>
-        <p className={styles.memberInfo}>
-          <span className={styles.label}>이메일:</span> {member.email}
-        </p>
-        <p className={styles.memberInfo}>
-          <span className={styles.label}>전화번호:</span> {member.phone}
-        </p>
-        <p className={styles.memberInfo}>
-          <span className={styles.label}>직책:</span> {member.position}
-        </p>
+        <div className={styles.myprofile}>
+          <div className={styles.info_title}>
+            <h3 className={styles.title_text}>기본정보</h3>
+          </div>
+          <ul className={styles.myinfo_area}>
+            <li>
+              <div className={styles.myphoto}>
+                <img
+                  src="https://static.nid.naver.com/images/web/user/default.png"
+                  width="56"
+                  height="56"
+                  alt="내 프로필 이미지"
+                />
+              </div>
+            </li>
+            <li>
+              <div className={styles.myaccount}>
+                <div className={styles.myname}>
+                  <div className={styles.name_text}>{member.name}</div>
+          
+                </div>
+                <div className={styles.myaddress}>{member.eid}</div>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-    );
-  };
+
+      {/* 부서 직위 */}
+      <div className={styles.subindex_box}>
+        <ul className={styles.subindex_row}>
+          <li>
+            <div className={`${styles.row_item} ${styles.bottom_line}`}>
+              <span className={styles.item_text}><FontAwesomeIcon className={styles.icon} icon={faBuilding} />부서 : </span>
+              <span className={styles.item_text}>{member.department}</span>
+            </div>
+          </li>
+          <li>
+            <div className={`${styles.row_item} ${styles.bottom_line}`}>
+              <span className={styles.item_text}><FontAwesomeIcon className={styles.icon}v icon={faRankingStar} />직위 : </span>
+              <span className={styles.item_text}>{member.position}</span>
+            </div>
+          </li>
+          <li>
+            <div className={`${styles.row_item} ${styles.bottom_line}`}>
+              <span className={styles.item_text}><FontAwesomeIcon  className={styles.icon} icon={faCalendarDays} />입사일 : </span>
+              <span className={styles.item_text}>{member.hireDate}</span>
+            </div>
+          </li>
+        </ul>
+      </div>
+
+      {/* 휴대전화 이메일 주소 */}
+      <div className={styles.subindex_box2}>
+        <ul className={styles.subindex_row}>
+          <li>
+            <div className={`${styles.row_item} ${styles.bottom_line}`}>
+              <span className={styles.item_text}> <FontAwesomeIcon className={styles.icon} icon={faPhone} />휴대전화 : </span>
+              <span className={styles.item_text}>{member.phone}</span>
+            </div>
+          </li>
+          <li>
+            <div className={`${styles.row_item} ${styles.bottom_line}`}>
+              <span className={styles.item_text}><FontAwesomeIcon className={styles.icon} icon={faEnvelope} />이메일 : </span>
+              <span className={styles.item_text}>{member.email}</span>
+            </div>
+          </li>
+
+          <li>
+            <div className={`${styles.row_item} ${styles.bottom_line}`}>
+              <span className={styles.item_text}><FontAwesomeIcon className={styles.icon} icon={faLocationDot} />주소 : </span>
+              <span className={styles.item_text}>{member.address}</span>
+            </div>
+          </li>
+        </ul>
+      </div>
 
 
-export default MemberReadComponent
+
+      <div className={styles.member_submit_btn_area}>
+        
+      <button className={styles.btn_edit}><Link to={`/member/modify/${member.eid}`}>회원 수정</Link></button>
+            <button className={styles.btn_edit}>회원 삭제</button>
+        
+      </div>
+    </div>
+  );
+};
+
+export default MemberReadComponent;
