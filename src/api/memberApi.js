03f7c6182c49347db7853aa03af7f960b0a6f4ac
MINJUN YU <minjoon0524@gmail.com>
2024-10-15 15:52:20 +0900

    feat:회원삭제 기능 추가

--- src/api/memberApi.js
+++ src/api/memberApi.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import jwtAxios from "./../util/jwtUtil";
 import { useState } from "react";
+import { getCookie } from "../util/CookieUtil";
 
 const API_SERVER_HOST = `http://localhost:80`;
 
@@ -27,11 +28,31 @@ export const getMemberList = async (pageParam) => {
     const res = await jwtAxios.get(`${API_SERVER_HOST}/member/list`, {
       params: { page, size, term: searchTerm, option: searchField },
     });
+   
     return res.data;
   } catch (error) {
     console.error("멤버 목록 API 오류:", error);
     throw error; // 오류를 다시 던져서 호출하는 곳에서 처리
   }
 
+};
 
+//eid를 통해 member를 읽어오는 함수
+export const getMember = async (eid) => {
+  const res = await jwtAxios.get(`${API_SERVER_HOST}/member/${eid}`);
+  console.log("getMember 함수 테스트 .... ", res.data);
+  return res.data;
+};
+
+export const modifyMember = async (member) => {
+  console.log("회원 수정 테스트")
+  console.log(member)
+  const res = await axios.put(`${API_SERVER_HOST}/member/modify`, member);
+  return res.data
+};
+
+export const deleteMember = async (eid) => {
+  console.log("회원 삭제 테스트")
+  const res = await jwtAxios.delete(`${API_SERVER_HOST}/member/${eid}`);
+  return res.data;
 };
