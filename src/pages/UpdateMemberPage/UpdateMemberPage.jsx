af89b16acc4d7fcb35f061770dae3c51c54bcba5
MINJUN YU <minjoon0524@gmail.com>
2024-10-16 20:47:52 +0900

    feat:사원상세정보 수정을 위한 페이지

--- src/pages/UpdateMemberPage/UpdateMemberPage.jsx
+++ src/pages/UpdateMemberPage/UpdateMemberPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import UpdateMemberComponent from '../../components/Main/UpdateMemberComponent/UpdateMemberComponent'
 import BasicLayout from '../../layouts/BasicLayout'
+import { useParams } from 'react-router-dom'
 
 const UpdateMemberPage = () => {
     const title= "인적사항 수정"
+    const { eid } = useParams();
     return (
       <BasicLayout title={title}>
-       <UpdateMemberComponent/>
+       <UpdateMemberComponent eid={eid}/>
       </BasicLayout>
     )
 }
