ed98d10cbb4dba2de583e92bfce58a442803e9ac
MINJUN YU <minjoon0524@gmail.com>
2024-10-19 15:39:59 +0900

    feat:사원 등록 API 호출완료

--- src/components/Join/JoinComponent.jsx
+++ src/components/Join/JoinComponent.jsx
@@ -1,46 +1,47 @@
-import React, { useState } from 'react'
-import style from "./JoinComponent.module.css"
-import Modal from 'react-modal';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faX } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from "react";
+import style from "./JoinComponent.module.css";
+import Modal from "react-modal";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faX } from "@fortawesome/free-solid-svg-icons";
+import { addMember } from "../../api/memberApi";
 
-const initState={
-  employeeNumber: '',
-  password: '',
-  name: '',
-  department: '',
-  position: '',
-  email: ''
-}
+const initState = {
+  eid: "",
+  pw: "",
+  name: "",
+  department: "",
+  position: "",
+  email: "",
+};
 
 const JoinComponent = ({ isOpen, onRequestClose }) => {
-  const [employeeData, setEmployeeData] = useState(initState);
+  const [member, setMember] = useState(initState);
 
   const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setEmployeeData(prevData => ({
-      ...prevData,
-      [name]: value
-    }));
+    member[e.target.name] = e.target.value
+    setMember({...member})
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // 여기에 사원 추가 로직을 구현하세요
-    console.log('사원 데이터:', employeeData);
+    addMember(member).then(data=>{
+      console.log("addMember Result : ",data)
+    })
     onRequestClose();
   };
 
   return (
     <Modal
-    isOpen={isOpen}
-    onRequestClose={onRequestClose}
-    className={style.modalContent}
-    overlayClassName={style.modalOverlay}
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      className={style.modalContent}
+      overlayClassName={style.modalOverlay}
     >
       <div className={style.container}>
         <div className={style.contents}>
-        <div><FontAwesomeIcon className={style.close_btn}  icon={faX} /></div>
+          <div className={style.close_btn}>
+            <FontAwesomeIcon onClick={onRequestClose} icon={faX} />
+          </div>
           <div className={style.contents_box}>
             {/* 첫번째 */}
             <div className={style.logo}>
@@ -51,65 +52,92 @@ const JoinComponent = ({ isOpen, onRequestClose }) => {
             {/* 두번째 */}
             <div className={style.logo}>
               <form onSubmit={handleSubmit}>
-                <div className={style.input_cover}>
-                  <input
-                    type="text"
-                    name="employeeNumber"
-                    className={style.input}
-                    placeholder="사번"
-                    value={employeeData.employeeNumber}
-                    onChange={handleInputChange}
-                  />
-                </div>
-                <div className={style.input_cover}>
-                  <input
-                    className={style.input}
-                    type='password'
-                    name="password"
-                    placeholder="비밀번호"
-                    value={employeeData.password}
-                    onChange={handleInputChange}
-                  />
-                </div>
-                <div className={style.input_cover}>
+
+              <div className={style.input_cover}>
                   <input
                     className={style.input}
                     name="name"
                     placeholder="이름"
-                    value={employeeData.name}
+                    value={member.name}
                     onChange={handleInputChange}
                   />
                 </div>
                 <div className={style.input_cover}>
                   <input
+                    type="text"
+                    name="eid"
+                    className={style.input}
+                    placeholder="사번"
+                    value={member.employeeNumber}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <div className={style.input_cover}>
+                  <input
+                    className={style.input}
+                    type="password"
+                    name="pw"
+                    placeholder="비밀번호"
+                    value={member.pw}
+                    onChange={handleInputChange}
+                  />
+                </div>
+     
+                <div className={style.input_cover}>
+                  <select
                     className={style.input}
                     name="department"
-                    placeholder="소속"
-                    value={employeeData.department}
+                    value={member.department}
                     onChange={handleInputChange}
-                  />
+                  >
+                    <option value="" disabled>
+                      부서
+                    </option>
+                    <option value="개발팀">개발팀</option>
+                    <option value="QA팀">QA팀</option>
+                    <option value="운영팀">운영팀</option>
+                    <option value="기술 지원팀">기술 지원팀</option>
+                    <option value="프로젝트 관리팀">프로젝트 관리팀</option>
+                    <option value="UX/UI 디자인팀">UX/UI 디자인팀</option>
+                    <option value="데이터 분석팀">데이터 분석팀</option>
+                    <option value="보안팀">보안팀</option>
+                    <option value="인프라팀">인프라팀</option>
+                    <option value="마케팅팀">마케팅팀</option>
+                  </select>
                 </div>
+
                 <div className={style.input_cover}>
-                  <input
-                    type='text'
+                  <select
                     className={style.input}
                     name="position"
-                    placeholder="직급"
-                    value={employeeData.position}
+                    value={member.position}
                     onChange={handleInputChange}
-                  />
+                  >
+                    <option value="" disabled>
+                      직급
+                    </option>
+                    <option value="사원">사원</option>
+                    <option value="주임">주임</option>
+                    <option value="대리">대리</option>
+                    <option value="과장">과장</option>
+                    <option value="차장">차장</option>
+                    <option value="부장">부장</option>
+                    <option value="이사">이사</option>
+                  </select>
                 </div>
                 <div className={style.input_cover}>
                   <input
-                    type='email'
+                    type="email"
                     className={style.input}
                     name="email"
                     placeholder="이메일"
-                    value={employeeData.email}
+                    value={member.email}
                     onChange={handleInputChange}
                   />
                 </div>
-                <button type="submit" className={style.btn_submit}>사원 추가</button>
+                <button type="submit" className={style.btn_submit}>
+                  사원 추가
+                </button>
               </form>
               {/* <button className={style.btn_close} onClick={onRequestClose}>닫기</button> */}
             </div>
@@ -117,7 +145,7 @@ const JoinComponent = ({ isOpen, onRequestClose }) => {
         </div>
       </div>
     </Modal>
-  )
-}
+  );
+};
 
-export default JoinComponent
+export default JoinComponent;
