43a5c37d50fee51f69fbe6da01c8cb1af1d53e01
MINJUN YU <minjoon0524@gmail.com>
2024-11-26 00:36:46 +0900

    feat: 수정시 사용하는 모달 추가

---
+++ src/modal/AlertModal.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './modal.module.css';
+
+const AlertModal = ({ open, onClose, title, message }) => {
+  if (!open) return null;
+
+  return (
+    <div className={styles.modal}>
+      <div className={styles.modalContent}>
+        <h2 className={styles.modalTitle}>{title}</h2>
+        <p className={styles.modalMessage}>{message}</p>
+        <div className={styles.modalButtonArea}>
+          <button onClick={onClose} className={styles.modalButtonConfirm}>
+            확인
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AlertModal;
