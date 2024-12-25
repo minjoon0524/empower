1d3ea0719e7bd3c90ca3c57f3b8279c878775fe7
MINJUN YU <minjoon0524@gmail.com>
2024-12-25 23:53:42 +0900

    feat: 쿠키유틸사용

--- src/util/jwtUtil.js
+++ src/util/jwtUtil.js
@@ -2,24 +2,48 @@ import axios from "axios";
 
 import { getCookie, setCookie } from "./CookieUtil";
 
+
 const jwtAxios = axios.create();
 
 // 엑세스 토큰을 가지고 전달해주는 axios
 // 아무것도 없이 전달하는 axios
+// const refreshJWT = async (accessToken, refreshToken) => {
+//   console.log("----------------------");
+//   console.log(accessToken)
+//   console.log(refreshToken)
+//   const host = "http://localhost";
+//   const header = {headers: {"Authorization":`Bearer ${accessToken}`}}
+//   const res = await axios.get(
+//     `${host}/refresh?refreshToken=${refreshToken}`,
+//     header
+//   );
+
+//   console.log("----------------------");
+//   console.log(res.data);
+
+//   // 새로만들어진 accessToken,refreshToken
+//   console.log(res.data);
+
+//   return res.data;
+// };
+
 const refreshJWT = async (accessToken, refreshToken) => {
   console.log("----------------------");
-  console.log(accessToken,refreshToken)
+  console.log(accessToken, refreshToken);
   const host = "http://localhost";
-  const header = {headers: {"Authorization":`Bearer ${accessToken}`}}
-  const res = await axios.get(
-    `${host}/refresh?refreshToken=${refreshToken}`,
-    header
+
+  // Authorization 헤더 설정
+  const header = { headers: { Authorization: `Bearer ${accessToken}` } };
+  console.log(header);
+
+  // POST 방식 요청
+  const res = await axios.post(
+    `${host}/refresh`, // URL 변경
+    { refreshToken }, // 요청 본문에 refreshToken 포함
+    header // 헤더 포함
   );
 
   console.log("----------------------");
-  console.log(res.data);
-
-  // 새로만들어진 accessToken,refreshToken
   console.log(res.data);
 
   return res.data;
@@ -30,6 +54,7 @@ const beforeReq = (config) => {
   console.log("before request.............");
 
   const memberInfo = getCookie("member");
+  console.log(memberInfo);
   if (!memberInfo) {
     console.log("Member NOT FOUND");
     return Promise.reject({ response: { data: { error: "REQUIRE_LOGIN" } } });
@@ -57,7 +82,8 @@ const beforeRes = async (res) => {
 
   if (data && data.error === "ERROR_ACCESS_TOKEN") {
     const memberCookieValue = getCookie("member");
-
+    console.log( memberCookieValue.accessToken)
+    console.log(memberCookieValue.refreshToken)
     const result = await refreshJWT(
       memberCookieValue.accessToken,
       memberCookieValue.refreshToken
@@ -67,7 +93,7 @@ const beforeRes = async (res) => {
     memberCookieValue.accessToken = result.accessToken;
     memberCookieValue.refreshToken = result.refreshToken;
 
-    setCookie("member", JSON.stringify(memberCookieValue), 1);
+    setCookie('member', JSON.stringify(memberCookieValue), 1);
 
     //원래의 호출
     const originalRequest = res.config;
